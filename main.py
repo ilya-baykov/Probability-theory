faf497f3882a8262d886b11338518a0b79640a19
Илья <ilya.baykov.1980@mail.ru>
2023-10-18 13:54:21 +0300

    Реализовал функции комбинаторики

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print(1)
+import math
+
+
+def permutations(n: int = 1) -> int:
+    return math.factorial(n)
+
+
+def combinations(k: int = 1, n: int = 2) -> int:
+    return math.factorial(n) // (math.factorial(n - k) * math.factorial(k))
+
+
+def placement(k: int = 1, n: int = 2) -> int:
+    return math.factorial(n) // math.factorial(n - k)
+
+
